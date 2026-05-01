06e517da6370bc26cb9a78ab8071784a22d8818d
rosamondlie <rosamond.patricia.01@gmail.com>
2026-05-01 08:21:26 +0700

    Initial Commit

---
+++ Tre(a)cker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tre(a)cker
+//
+//  Created by Rosamond Patricia Selamat Lie on 01/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tre(a)cker/Tre_a_ckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tre_a_ckerApp.swift
+//  Tre(a)cker
+//
+//  Created by Rosamond Patricia Selamat Lie on 01/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Tre_a_ckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
