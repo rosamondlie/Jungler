cf0805c37681bfa68bcfc3f9915e95b20fd13010
neena <neenaardiena@gmail.com>
2026-05-04 14:02:24 +0700

    neena update file di folder view

---
+++ Tre(a)cker/View/neenaView.swift
@@ -0,0 +1,18 @@
+//
+//  neenaView.swift
+//  Tre(a)cker
+//
+//  Created by neena on 04/05/26.
+//
+
+import SwiftUI
+
+struct neenaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    neenaView()
+}
